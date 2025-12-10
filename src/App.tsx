1847c80ef09739e5592a55057eb372b58faaf0d0
Kevin Valenzuela <kevintores92@gmail.com>
2025-12-10 14:40:53 +0800

    ui

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { AppLayout } from "@/components/layout/AppLayout";
+import Messenger from "@/pages/Messenger";
+import Campaigns from "@/pages/Campaigns";
+import Contacts from "@/pages/Contacts";
+import Settings from "@/pages/Settings";
+import NotFound from "@/pages/not-found";
+
+function Router() {
+  return (
+    <AppLayout>
+      <Switch>
+        <Route path="/" component={Messenger} />
+        <Route path="/inbox" component={Messenger} />
+        <Route path="/campaigns" component={Campaigns} />
+        <Route path="/contacts" component={Contacts} />
+        <Route path="/settings" component={Settings} />
+        <Route component={NotFound} />
+      </Switch>
+    </AppLayout>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
